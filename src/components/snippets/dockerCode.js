d9acb61c5f841aad40a734c8a77fa56b9ff5e312
rao-vasireddy <38476511+rao-vasireddy@users.noreply.github.com>
2019-10-28 15:57:26 -0700

    change wget of yb-docker-ctl from s3 downloads bucket to github

--- src/components/snippets/dockerCode.js
+++ src/components/snippets/dockerCode.js
@@ -1,5 +1,5 @@
 export const dbServerCode = `
-wget https://downloads.yugabyte.com/yb-docker-ctl
+wget https://raw.githubusercontent.com/yugabyte/yugabyte-db/master/bin/yb-docker-ctl 
 chmod +x yb-docker-ctl
 ./yb-docker-ctl create
 `
