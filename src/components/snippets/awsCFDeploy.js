11202ecbfc9eefb429c98bca92ebdda8cf7dce93
rao-vasireddy <38476511+rao-vasireddy@users.noreply.github.com>
2019-12-04 18:06:52 -0800

    Update version to 2.0.7.0

--- src/components/snippets/awsCFDeploy.js
+++ src/components/snippets/awsCFDeploy.js
@@ -1,6 +1,6 @@
 const code = `
 wget https://raw.githubusercontent.com/yugabyte/aws-cloudformation/master/yugabyte_cloudformation.yaml
-aws cloudformation create-stack --stack-name <stack-name> --template-body yugabyte_cloudformation.yaml --parameters DBVersion=2.0.6.0, KeyName=<ssh-key-name>
+aws cloudformation create-stack --stack-name <stack-name> --template-body yugabyte_cloudformation.yaml --parameters DBVersion=2.0.7.0, KeyName=<ssh-key-name>
 `
 
 export default code
