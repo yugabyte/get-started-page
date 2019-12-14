ebb6d9b025af53d01ae9f0e565c6f6af972225fe
rao-vasireddy <38476511+rao-vasireddy@users.noreply.github.com>
2019-12-14 08:51:40 -0800

    Update version to 2.0.8.0

--- src/components/snippets/linuxCode.js
+++ src/components/snippets/linuxCode.js
@@ -1,6 +1,6 @@
 export const dbServerCode = `
-wget https://downloads.yugabyte.com/yugabyte-2.0.7.0-linux.tar.gz
-tar xvfz yugabyte-2.0.7.0-linux.tar.gz && cd yugabyte-2.0.7.0/
+wget https://downloads.yugabyte.com/yugabyte-2.0.8.0-linux.tar.gz
+tar xvfz yugabyte-2.0.8.0-linux.tar.gz && cd yugabyte-2.0.8.0/
 ./bin/post_install.sh
 ./bin/yb-ctl create
 `
