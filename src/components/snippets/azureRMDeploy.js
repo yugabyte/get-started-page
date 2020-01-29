caebae1fd94799c4b26591ea29bd7ce4075aa2ca
rao-vasireddy <38476511+rao-vasireddy@users.noreply.github.com>
2020-01-28 18:16:10 -0800

    Update version to 2.0.11.0

--- src/components/snippets/azureRMDeploy.js
+++ src/components/snippets/azureRMDeploy.js
@@ -3,7 +3,7 @@ ssh-keygen -f yugabyte-key
 git clone https://github.com/YugaByte/azure-resource-manager.git
 cd azure-resource-manager
 az group create --name yb-demo -l <region>
-az group deployment create --resource-group yb-demo --template-file yugabyte_deployment.json --parameters ClusterName='yb-demo' SshUser='centos' YBVersion='2.0.10.0' SshKeypair='<contents of yugabyte-key.pub>'
+az group deployment create --resource-group yb-demo --template-file yugabyte_deployment.json --parameters ClusterName='yb-demo' SshUser='centos' YBVersion='2.0.11.0' SshKeypair='<contents of yugabyte-key.pub>'
 `
 
 export default code
