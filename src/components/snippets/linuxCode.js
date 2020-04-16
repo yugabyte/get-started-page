8abce450788544d4fc8c5da46202f6f71125586c
rao-vasireddy <38476511+rao-vasireddy@users.noreply.github.com>
2020-04-15 21:13:57 -0700

    Update version to 2.1.4.0

--- src/components/snippets/linuxCode.js
+++ src/components/snippets/linuxCode.js
@@ -1,6 +1,6 @@
 export const dbServerCode = `
-wget https://downloads.yugabyte.com/yugabyte-2.1.3.0-linux.tar.gz
-tar xvfz yugabyte-2.1.3.0-linux.tar.gz && cd yugabyte-2.1.3.0/
+wget https://downloads.yugabyte.com/yugabyte-2.1.4.0-linux.tar.gz
+tar xvfz yugabyte-2.1.4.0-linux.tar.gz && cd yugabyte-2.1.4.0/
 ./bin/post_install.sh
 ./bin/yb-ctl create
 `
