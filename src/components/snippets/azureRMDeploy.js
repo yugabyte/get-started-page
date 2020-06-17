2dd2fb4bcbc4ec95efaf824a921a52ded6a0b74d
Steve Bang <stevebang@gmail.com>
2020-06-16 17:58:43 -0700

    Update version to 2.1.8.1

--- src/components/snippets/azureRMDeploy.js
+++ src/components/snippets/azureRMDeploy.js
@@ -3,7 +3,7 @@ ssh-keygen -f yugabyte-key
 git clone https://github.com/yugabyte/azure-resource-manager.git
 cd azure-resource-manager
 az group create --name yb-demo -l <region>
-az group deployment create --resource-group yb-demo --template-file yugabyte_deployment.json --parameters ClusterName='yb-demo' SshUser='centos' YBVersion='2.1.6.0' SshKeypair='<contents of yugabyte-key.pub>'
+az group deployment create --resource-group yb-demo --template-file yugabyte_deployment.json --parameters ClusterName='yb-demo' SshUser='centos' YBVersion='2.1.8.1' SshKeypair='<contents of yugabyte-key.pub>'
 `
 
 export default code
