fc937dbc93e642bce511712d5bc7dcf330f19915
Steve Bang <stevebang@gmail.com>
2020-09-09 00:27:22 -0700

    Update to 2.3.0.0

--- src/components/snippets/linuxCode.js
+++ src/components/snippets/linuxCode.js
@@ -1,6 +1,6 @@
 export const dbServerCode = `
-wget https://downloads.yugabyte.com/yugabyte-2.2.2.0-linux.tar.gz
-tar xvfz yugabyte-2.2.2.0-linux.tar.gz && cd yugabyte-2.2.2.0/
+wget https://downloads.yugabyte.com/yugabyte-2.3.0.0-linux.tar.gz
+tar xvfz yugabyte-2.3.0.0-linux.tar.gz && cd yugabyte-2.3.0.0/
 ./bin/post_install.sh
 ./bin/yugabyted start
 `
