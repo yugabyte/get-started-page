f6eaea5bc213a76fec120e2955085bfa69bd3f7e
Andrew Cai <andrew@yugabyte.com>
2021-06-14 16:08:29 -0700

    Fix eslint error during build run

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,14 +1,17 @@
 export const copyToClipboard = function (text) {
-  const textArea = document.createElement('textarea')
-  textArea.value = text
-  document.body.appendChild(textArea)
-  textArea.focus({ preventScroll: true })
-  textArea.select()
-
-  try {
-    document.execCommand('copy')
-  } catch (err) {
-    console.error('Unable to copy to clipboard')
+  if (!window.navigator || !navigator.clipboard) {
+    const textArea = document.createElement('textarea')
+    textArea.value = text
+    document.body.appendChild(textArea)
+    textArea.focus({ preventScroll: true })
+    textArea.select()
+    try {
+      document.execCommand('copy')
+    } catch (err) {
+      // TODO: Add fallback for copying text
+    }
+    document.body.removeChild(textArea)
+    return
   }
-  document.body.removeChild(textArea)
+  navigator.clipboard.writeText(text)
 }
