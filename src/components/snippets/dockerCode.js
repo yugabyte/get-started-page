b4c5ce9c02102c8478d75459b7e81f9166fc2c19
Dwight Hodge <ghodge@yugabyte.com>
2023-10-12 20:59:02 -0400

    Update Docker code

--- src/components/snippets/dockerCode.js
+++ src/components/snippets/dockerCode.js
@@ -1,12 +1,12 @@
 export const dbServerCode = (version) => `
 docker pull yugabytedb/yugabyte:${version}
-docker run -d --name yugabyte  -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042\
+docker run -d --name yugabyte  -p7000:7000 -p9000:9000 -p15433:15433 -p5433:5433 -p9042:9042\
  yugabytedb/yugabyte:${version} bin/yugabyted start\
  --daemon=false --ui=false
 `;
 
 export const sqlShellCode = `
-docker exec -it yugabyte /home/yugabyte/bin/ysqlsh
+docker exec -it yugabyte bash -c '/home/yugabyte/bin/ysqlsh --echo-queries --host $(hostname)'
 `;
 
 export const pgCommands = `
