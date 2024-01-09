d39076487ea78a5f346c57e6595a5571bba5ae5e
aishwarya24 <ashchakravarthy@gmail.com>
2024-01-09 15:55:21 -0500

    delete daemon flag

--- src/components/snippets/dockerCode.js
+++ src/components/snippets/dockerCode.js
@@ -1,8 +1,7 @@
 export const dbServerCode = (version) => `
 docker pull yugabytedb/yugabyte:${version}
 docker run -d --name yugabyte -p7000:7000 -p9000:9000 -p15433:15433 -p5433:5433 -p9042:9042 \
- yugabytedb/yugabyte:${version} bin/yugabyted start \
- --daemon=false
+ yugabytedb/yugabyte:${version} bin/yugabyted start
 `;
 
 export const sqlShellCode = `
