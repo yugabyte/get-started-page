dd48f585a212470c1666fd55c6a90fdc5e1f20b1
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2024-05-09 17:03:07 +0500

    Integrate segment with dynamic key based on netlify context

--- quasar.config.js
+++ quasar.config.js
@@ -48,6 +48,9 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
+      env: {
+        SEGMENT_API_KEY: process.env.SEGMENT_API_KEY
+      },
       target: {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16',
