499d8a43ae89354705ff086e18ad0216567e1514
Dwight Hodge <ghodge@yugabyte.com>
2025-01-03 16:48:37 -0500

    docker download url

--- src/components/snippets/dockerCode.js
+++ src/components/snippets/dockerCode.js
@@ -1,5 +1,5 @@
 export const dbServerCode = (version) => `
-docker pull yugabytedb/yugabyte:${version}
+docker pull software.yugabyte.com/yugabytedb/yugabyte:${version}
 docker run -d --name yugabyte -p7000:7000 -p9000:9000 -p15433:15433 -p5433:5433 -p9042:9042 \
  yugabytedb/yugabyte:${version} bin/yugabyted start \
  --background=false
