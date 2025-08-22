fcf31e6afa779fcbcc4a42ae94cd9b316c4e8d5e
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2025-08-22 15:49:49 +0500

    history and hash w.r.t config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,16 @@
 import { route } from 'quasar/wrappers';
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from 'vue-router';
 import routes from './routes';
 
 export default route(function () {
+  const createHistory = process.env.VUE_ROUTER_MODE === 'history'
+    ? createWebHistory
+    : createWebHashHistory;
+
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes: routes as RouteRecordRaw[],
-    history: createWebHistory(process.env.VUE_ROUTER_BASE),
+    history: createHistory(process.env.VUE_ROUTER_BASE),
   });
 
   return Router;
